4b8d213f5eb7f7a8a8164ec1569719c270d3f4c2
Fahmi Nurfikri <fahmisalman@gmail.com>
2023-09-26 20:36:28 +0700

    :sparkles: add log in verification

--- pkg/auth/login.go
+++ pkg/auth/login.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
+	"go_auth/pkg/common/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginInput struct {
@@ -11,11 +13,27 @@ type LoginInput struct {
 	Password string `json:"password" binding:"required"`
 }
 
+func verifyPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
 func (h handler) Login(c *gin.Context) {
 	var input LoginInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := models.User{}
+
+	if err := h.DB.Model(models.User{}).Where("username = ?", input.Username).Take(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username and password not valid"})
+		return
+	}
+
+	if err := verifyPassword(input.Password, user.Password); err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username and password not valid"})
 		return
 	}
 
--- pkg/common/db/db.go
+++ pkg/common/db/db.go
@@ -12,7 +12,7 @@ import (
 
 func InitDB() *gorm.DB {
 
-	err := godotenv.Load(".env")
+	err := godotenv.Load("pkg/common/envs/.env")
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
