557af817362fd134e5af902c78bb262c9fd49966
Fahmi Nurfikri <fahmisalman@gmail.com>
2023-10-11 20:48:32 +0700

    feat: :sparkles: add swagger documentation

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	_ "go_auth/docs"
 	app "go_auth/pkg/app"
 	auth "go_auth/pkg/auth"
 	db "go_auth/pkg/common/db"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +15,9 @@ import (
 func main() {
 	database := db.InitDB()
 	router := gin.Default()
+
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	auth.RegisterRoutes(router, database)
 	app.RegisterRoutes(router, database)
--- pkg/auth/profile.go
+++ pkg/auth/profile.go
@@ -10,6 +10,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary Get the current user
+// @Description Get information about the currently authenticated user.
+// @Produce json
+// @Success 200 {object} CurrentUserResponse
+// @Failure 400 {object} ErrorResponse
+// @Router /auth/profile [get]
 func (h handler) CurrentUser(c *gin.Context) {
 	userID, tokenErr := token.ExtractTokenID(c)
 	if tokenErr != nil {
@@ -24,6 +30,25 @@ func (h handler) CurrentUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "success", "data": currentUser})
+}
+
+// CurrentUserResponse represents the response structure for the CurrentUser endpoint.
+type CurrentUserResponse struct {
+	Message string          `json:"message"`
+	Data    CurrentUserData `json:"data"`
+}
+
+// CurrentUserData represents the information about the current user.
+type CurrentUserData struct {
+	ID       uint   `json:"id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
 }
 
 func (h handler) getCurrentUser(userID uint) (*models.User, error) {
